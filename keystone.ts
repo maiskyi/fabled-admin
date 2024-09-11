fc6721f85413c01f58538a266bb06ecb035ecc1e
Andrii Maiskyi <amaiskyi@bellum.ai>
2024-09-11 20:45:51 +0100

    feat: cors

--- keystone.ts
+++ keystone.ts
@@ -5,6 +5,9 @@ import { withAuth, session } from "./src/_auth";
 
 export default withAuth(
   config({
+    server: {
+      cors: { origin: ["http://localhost:5173"] },
+    },
     db: {
       provider: "sqlite",
       url: "file:./keystone.db",
