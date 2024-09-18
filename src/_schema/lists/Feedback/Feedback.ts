ed1ee7b678625dcca172a8252a70d342e0b7ab8f
Andrii Maiskyi <amaiskyi@bellum.ai>
2024-09-18 13:27:34 +0100

    feat: api

--- src/_schema/lists/Feedback/Feedback.ts
+++ src/_schema/lists/Feedback/Feedback.ts
@@ -5,7 +5,22 @@ import { text, timestamp, integer } from "@keystone-6/core/fields";
 export const Feedback = list({
   access: allowAll,
   fields: {
+    firebaseUserId: text({
+      ui: {
+        itemView: {
+          fieldMode: "read",
+        },
+      },
+      validation: {
+        isRequired: true,
+      },
+    }),
     rating: integer({
+      ui: {
+        itemView: {
+          fieldMode: "read",
+        },
+      },
       validation: {
         isRequired: true,
         min: 0,
@@ -15,12 +30,20 @@ export const Feedback = list({
     comment: text({
       ui: {
         displayMode: "textarea",
+        itemView: {
+          fieldMode: "read",
+        },
       },
       validation: {
         isRequired: true,
       },
     }),
     createdAt: timestamp({
+      ui: {
+        itemView: {
+          fieldMode: "read",
+        },
+      },
       defaultValue: {
         kind: "now",
       },
