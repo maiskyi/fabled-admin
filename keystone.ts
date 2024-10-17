7af0bc1c7472db4039aa84d2c067ddafded04d21
Andrii Maiskyi <amaiskyi@bellum.ai>
2024-10-17 12:10:40 +0100

    feat: deploy

--- keystone.ts
+++ keystone.ts
@@ -4,17 +4,15 @@ import { lists, extendGraphqlSchema } from "./src/_schema";
 import { withAuth, session } from "./src/_auth";
 import { extendHttpServer } from "./src/_server";
 
-const SERVER_CORS_ORIGINS = process.env.SERVER_CORS_ORIGINS || "";
-
 export default withAuth(
   config({
     server: {
       extendHttpServer,
       cors: {
-        origin: SERVER_CORS_ORIGINS.split(",")
+        origin: (process.env.SERVER_CORS_ORIGINS || "")
+          .split(",")
           .map((v) => v.trim())
-          .filter((v) => !!v)
-          .concat(["capacitor://localhost"]),
+          .filter((v) => !!v),
       },
     },
     db: {
