08081bbc8ad303c5c181120dd7fac0d378cbd4f0
Andrii Maiskyi <amaiskyi@bellum.ai>
2024-11-06 02:30:30 +0000

    feat: auth

--- src/_schema/lists/Story/access/operation/create/index.ts
+++ src/_schema/lists/Story/access/operation/create/index.ts
@@ -2,9 +2,21 @@ import { ListAccessControl } from "@keystone-6/core/types";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Lists } from ".keystone/types";
 import { PartialDeep } from "type-fest";
+import { Session } from "../../../../lists.types";
+import { Auth } from "../../../../../../_firebase";
 
-export const create: PartialDeep<ListAccessControl<Lists.Story.TypeInfo>> = {
+export const create: PartialDeep<
+  ListAccessControl<Lists.Story.TypeInfo<Session>>
+> = {
   operation: {
-    create: async () => true,
+    create: async ({ session, context }) => {
+      if (session?.data?.isAdmin) return true;
+
+      const authorization = context.req?.headers.authorization;
+
+      const { uid } = await Auth.verifyIdToken(authorization);
+
+      return !!uid;
+    },
   },
 };
--- src/_schema/lists/lists.types.ts
+++ src/_schema/lists/lists.types.ts
@@ -1,3 +1,9 @@
 export enum Language {
   English = "en",
 }
+
+export interface Session {
+  data?: {
+    isAdmin: boolean;
+  };
+}
