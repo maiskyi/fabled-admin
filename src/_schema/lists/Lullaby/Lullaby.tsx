709d8c1a74fca247882a41b3242c46e5cab86914
Andrii Maiskyi <andrey@wearehelful.com>
2025-02-12 00:58:51 +0000

    feat: lullaby. #184

--- src/_schema/lists/Lullaby/Lullaby.tsx
+++ src/_schema/lists/Lullaby/Lullaby.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Lists } from ".keystone/types";
 import { list } from "@keystone-6/core";
-import { file, text } from "@keystone-6/core/fields";
+import { checkbox, file, text, timestamp } from "@keystone-6/core/fields";
 import { allowAll } from "@keystone-6/core/access";
 import { Session } from "../lists.types";
 
@@ -19,5 +19,19 @@ export const Lullaby = list<Lists.Config.TypeInfo<Session>>({
       },
     }),
     mp3: file({ storage: "lullabies" }),
+    isPublished: checkbox(),
+    createdAt: timestamp({
+      ui: {
+        itemView: {
+          fieldMode: "read",
+        },
+        createView: {
+          fieldMode: "hidden",
+        },
+      },
+      defaultValue: {
+        kind: "now",
+      },
+    }),
   },
 });
